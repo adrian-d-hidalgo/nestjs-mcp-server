1a09a3dda3df8a448fde3bc9a2ba870ef669b406
Adrián Darío Hidalgo Flores <adrian.hidalgo@mailfusion.me>
2025-05-05 23:59:21 -0600

    docs: add documentation about extra arg

--- examples/guards/guards.resolver.ts
+++ examples/guards/guards.resolver.ts
@@ -2,29 +2,37 @@ import {
   CallToolResult,
   GetPromptResult,
 } from '@modelcontextprotocol/sdk/types';
-import { CanActivate, Injectable } from '@nestjs/common';
+import { CanActivate } from '@nestjs/common';
 import { z } from 'zod';
 
 import {
   McpExecutionContext,
   Prompt,
+  RequestHandlerExtra,
   Resolver,
   Tool,
   UseGuards,
 } from '../../src';
 
 // Resolver-level guard
-@Injectable()
+// Applies to all methods in the resolver
 export class ResolverLogGuard implements CanActivate {
-  canActivate(_context: McpExecutionContext): boolean {
+  canActivate(context: McpExecutionContext): boolean {
     console.log('[ResolverLogGuard] Resolver-level guard executed');
+
+    console.log('sessionId', context.getSessionId());
+    // Args as a structured object, mapped by parameter names for predictable access
+    console.log('headers', context.args);
+
+    // Args as a raw array, ordered as passed to the resolver method
+    console.log('extra', context.getArgs());
 
     return true;
   }
 }
 
 // Method-level guard
-@Injectable()
+// Applies to a single method in the resolver but it has the same context as the resolver-level guard
 export class MethodLogGuard implements CanActivate {
   canActivate(_context: McpExecutionContext): boolean {
     console.log('[MethodLogGuard] Method-level guard executed');
@@ -37,7 +45,7 @@ export class MethodLogGuard implements CanActivate {
 @Resolver('guards')
 export class GuardsResolver {
   @Prompt({ name: 'logPrompt' })
-  logPrompt(): GetPromptResult {
+  logPrompt(_extra: RequestHandlerExtra): GetPromptResult {
     return {
       messages: [
         {
@@ -55,7 +63,10 @@ export class GuardsResolver {
       prefix: z.string(),
     },
   })
-  logTool(args: { prefix: string }): CallToolResult {
+  logTool(
+    args: { prefix: string },
+    _extra: RequestHandlerExtra,
+  ): CallToolResult {
     return {
       content: [{ type: 'text', text: `[${args.prefix}] Tool executed` }],
     };
